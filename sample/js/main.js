2cdb4a2970964fe211d37d197a33dd3b22841334
dwarakatraders <135849131+dwarakatraders@users.noreply.github.com>
2023-06-25 13:12:59 +0530

    update files

--- sample/js/main.js
+++
@@ -1,8 +0,0 @@
-// Script to open and close sidebar
-function dt_open() {
-  document.getElementById("mySidebar").style.display = "block";
-}
- 
-function dt_close() {
-  document.getElementById("mySidebar").style.display = "none";
-}
